23ab21bbf285024de430772797564f4b721cfedd
vipin2060 <vipinvikraman86@gmail.com>
2025-06-02 11:17:09 +0800

    Create outputs.tf
    Uploading output file

---
+++ outputs.tf
@@ -0,0 +1,82 @@
+
+output "TGW_ID_Parent" {
+  value = module.Deploy-DualRegion-TGW-01.TGW_ID
+}
+
+
+output "TGW_OWNER_ID_Parent" {
+  value = module.Deploy-DualRegion-TGW-01.TGW_OWNER_ID
+}
+
+output "TGW_DEFAULT_RT_ID_Parent" {
+  value = module.Deploy-DualRegion-TGW-01.TGW_DEFAULT_RT_ID
+}
+
+output "TGW_DEFAULT_RT_ID_Propgation_Parent" {
+  value = module.Deploy-DualRegion-TGW-01.TGW_DEFAULT_RT_ID_Propgation
+}
+
+
+output "TGW_AUTO_ACCEPT_SHARED_ATTACHMENTS_Parent" {
+  value = module.Deploy-DualRegion-TGW-01.TGW_AUTO_ACCEPT_SHARED_ATTACHMENTS
+}
+
+
+output "TGW_DEFAULT_ROUTETABLE_ASSOCIATIONS_Parent" {
+  value = module.Deploy-DualRegion-TGW-01.TGW_DEFAULT_ROUTETABLE_ASSOCIATIONS
+}
+
+
+output "TGW_PROPAGATIONS_Parent" {
+  value = module.Deploy-DualRegion-TGW-01.TGW_PROPAGATIONS
+}
+
+output "TGW_DESCRIPTION_Parent" {
+  value = module.Deploy-DualRegion-TGW-01.TGW_DESCRIPTION
+}
+
+output "TGW_NAME_Parent" {
+  value = module.Deploy-DualRegion-TGW-01.TGW_NAME
+}
+
+# for module 2 
+
+output "TGW_ID_Parent_2" {
+  value = module.Deploy-DualRegion-TGW-02.TGW_ID
+}
+
+
+output "TGW_OWNER_ID_Parent_2" {
+  value = module.Deploy-DualRegion-TGW-02.TGW_OWNER_ID
+}
+
+output "TGW_DEFAULT_RT_ID_Parent_2" {
+  value = module.Deploy-DualRegion-TGW-02.TGW_DEFAULT_RT_ID
+}
+
+output "TGW_DEFAULT_RT_ID_Propgation_Parent_2" {
+  value = module.Deploy-DualRegion-TGW-02.TGW_DEFAULT_RT_ID_Propgation
+}
+
+
+output "TGW_AUTO_ACCEPT_SHARED_ATTACHMENTS_Parent_2" {
+  value = module.Deploy-DualRegion-TGW-02.TGW_AUTO_ACCEPT_SHARED_ATTACHMENTS
+}
+
+
+output "TGW_DEFAULT_ROUTETABLE_ASSOCIATIONS_Parent_2" {
+  value = module.Deploy-DualRegion-TGW-02.TGW_DEFAULT_ROUTETABLE_ASSOCIATIONS
+}
+
+
+output "TGW_PROPAGATIONS_Parent_2" {
+  value = module.Deploy-DualRegion-TGW-02.TGW_PROPAGATIONS
+}
+
+output "TGW_DESCRIPTION_Parent_2" {
+  value = module.Deploy-DualRegion-TGW-02.TGW_DESCRIPTION
+}
+
+output "TGW_NAME_Parent_2" {
+  value = module.Deploy-DualRegion-TGW-02.TGW_NAME
+}
